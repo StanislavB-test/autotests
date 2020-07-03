2e1ce741754c873a4d5207a26ed1792f34eae17e
StanislavB-test <55495563+StanislavB-test@users.noreply.github.com>
2020-07-03 11:12:00 +0300

    Add files via upload

--- autoPieAndHistHier.py
+++ autoPieAndHistHier.py
@@ -4,7 +4,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 
-class PivotAndTableHier(unittest.TestCase): #Проверяем иерархию из шапки и значений таблицы и таблицы среза
+class PivotAndTableHier(unittest.TestCase): #Проверяем иерархию из круговой и гистограммы. Различные наборы
 
     def wait_by_css(self, element_locator):
         WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, element_locator)))
@@ -20,28 +20,32 @@ class PivotAndTableHier(unittest.TestCase): #Проверяем иерархию
         self.driver.find_element_by_css_selector("#username").send_keys("netrika")
         self.driver.find_element_by_css_selector("#password").send_keys("netrika")
         self.driver.find_element_by_css_selector("input.btn").click()
-        self.driver.get('http://ss.dev.bnvt.ru/superset/dashboard/169')
+        self.driver.get('http://ss.dev.bnvt.ru/superset/dashboard/178')
 
     def hierPie1(self):
         actions = webdriver.ActionChains(self.driver)
-        self.wait_by_css('#slice-container-698 > table > thead > tr:nth-child(3) > th:nth-child(1)')
-        clickPoint = self.driver.find_element_by_css_selector('#slice-container-698 > '
-                                                              'table > thead > tr:nth-child(3) > th:nth-child(1)')
+        self.wait_by_css('#slice-container-746 > svg > g > g > g.nv-pieWrap.nvd3-svg > g > g > g.nv-pie > '
+                         'g:nth-child(1)')
+        clickPoint = self.driver.find_element_by_css_selector('#slice-container-746 > svg > g > g > '
+                                                              'g.nv-pieWrap.nvd3-svg > g > g > g.nv-pie > '
+                                                              'g:nth-child(1)')
         actions.move_to_element(clickPoint).context_click().perform()
         self.driver.find_element_by_css_selector(
             '.d3-context-menu > ul:nth-child(1) > li:nth-child(2)').click()  # клик по меню
-        self.wait_by_css('#slice-container-698 > table > thead > tr:nth-child(3) > th:nth-child(2)')
-        result = self.driver.find_element_by_css_selector('#slice-container-698 > '
-                                                          'table > thead > tr:nth-child(3) > th:nth-child(2)').text
+        self.wait_by_css('#slice-container-746 > svg > g > g > g.nv-pieWrap.nvd3-svg > g > g > g.nv-pie > '
+                         'g:nth-child(1) > path') #Ждем пока прогрузится диаграмма
+        result = self.driver.find_element_by_css_selector('#slice-container-746 > svg:nth-child(1) > g:nth-child(1) > '
+                                                          'g:nth-child(1) > g:nth-child(2) > g:nth-child(1) > '
+                                                          'g:nth-child(1) > g:nth-child(2) > text:nth-child(2)').text
+        #Проверка значения по шапке
         print(result)
-        assert result == 'case_organization_level1_short_name', 'Упало при переходе из шапки hierPivot1'  # проверка появившегося поля
-        # case_patient_gender в среза
-
+        assert result == 'Невский район,ГП №100', 'Упало при переходе из шапки hierPie1'  # проверка появившегося поля
 
 
     def test_case(self):
         self.setup()
         self.login()
+        self.hierPie1()
 
 
 
